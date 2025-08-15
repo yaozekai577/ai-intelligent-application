074ad7358cdca620e82f59431bec99e583d410ce
itcast <hello@itcast.cn>
2025-08-15 20:20:27 +0800

    会话历史功能

--- src/main/java/com/heima/ai/controller/ChatController.java
+++ src/main/java/com/heima/ai/controller/ChatController.java
@@ -1,5 +1,6 @@
 package com.heima.ai.controller;
 
+import com.heima.ai.repository.ChatHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
@@ -16,8 +17,13 @@ public class ChatController {
 
     private final ChatClient chatClient;
 
+    private final ChatHistoryRepository chatHistoryRepository;
+
     @RequestMapping(value = "/chat",produces = "text/html;charset=utf-8")
     public Flux<String> chat(String prompt,String chatId){
+        //1.保存会话id
+        chatHistoryRepository.save("chat",chatId);
+        //2.请求会话模型
         return chatClient.prompt()
                 .user(prompt)
                 .advisors(a->a.param(ChatMemory.CONVERSATION_ID,chatId))
