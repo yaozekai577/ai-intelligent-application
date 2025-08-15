7518abf077209dc0824baced2cb2065c57d76d94
itcast <hello@itcast.cn>
2025-08-15 21:15:01 +0800

    之前可能看漏了，这可能是有问题

--- src/main/java/com/heima/ai/config/CommonConfiguration.java
+++ src/main/java/com/heima/ai/config/CommonConfiguration.java
@@ -23,12 +23,12 @@ public class CommonConfiguration {
     }
 
     @Bean
-    public ChatClient chatClient(OllamaChatModel model) {
+    public ChatClient chatClient(OllamaChatModel model,ChatMemory chatMemory) {
         return ChatClient
                 .builder(model)
                 .defaultSystem("你是一个热心、可爱的助手，你的名字叫小团团，请以小团团的身份和语气来回答问题")
                 .defaultAdvisors(new SimpleLoggerAdvisor())//日志
-                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory()).build())//会话记忆
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())//会话记忆
                 .build();
     }
 }
