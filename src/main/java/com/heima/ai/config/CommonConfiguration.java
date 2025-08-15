ab79a799dc87ab7e0f6b66ea75deadd149fbea81
itcast <hello@itcast.cn>
2025-08-15 21:44:48 +0800

    哄哄模拟器配置

--- src/main/java/com/heima/ai/config/CommonConfiguration.java
+++ src/main/java/com/heima/ai/config/CommonConfiguration.java
@@ -1,5 +1,6 @@
 package com.heima.ai.config;
 
+import com.heima.ai.constants.SystemConstants;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
@@ -7,6 +8,7 @@ import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,19 +16,43 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CommonConfiguration {
 
-
-
+    /**
+     * 配置会话记忆存储
+     * @return
+     */
     @Bean
     public ChatMemory chatMemory() {
         // 使用 MessageWindowChatMemory 作为默认内存策略（窗口消息保留）
         return MessageWindowChatMemory.builder().build();
     }
 
+    /**
+     * AI对话用ChatClient对象，用于处理用户输入的文本，并返回处理结果
+     * @param model
+     * @param chatMemory
+     * @return
+     */
     @Bean
     public ChatClient chatClient(OllamaChatModel model,ChatMemory chatMemory) {
         return ChatClient
                 .builder(model)
                 .defaultSystem("你是一个热心、可爱的助手，你的名字叫小团团，请以小团团的身份和语气来回答问题")
+                .defaultAdvisors(new SimpleLoggerAdvisor())//日志
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())//会话记忆
+                .build();
+    }
+
+    /**
+     * 哄哄模拟器游戏用ChatClient对象，用于模拟女友进行游戏
+     * @param model
+     * @param chatMemory
+     * @return
+     */
+    @Bean
+    public ChatClient gameChatClient(OpenAiChatModel model, ChatMemory chatMemory) {
+        return ChatClient
+                .builder(model)
+                .defaultSystem(SystemConstants.GAME_SYSTEM_PROMPT)//系统配置
                 .defaultAdvisors(new SimpleLoggerAdvisor())//日志
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())//会话记忆
                 .build();
