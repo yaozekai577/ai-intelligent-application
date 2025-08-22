987ec467562a0bf70b9a9f673147f61b888e40a2
itcast <hello@itcast.cn>
2025-08-22 23:01:09 +0800

    测试向量模型

--- src/test/java/com/heima/ai/HeimaAiApplicationTests.java
+++ src/test/java/com/heima/ai/HeimaAiApplicationTests.java
@@ -1,13 +1,57 @@
 package com.heima.ai;
 
+import com.heima.ai.util.VectorDistanceUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootTest
 class HeimaAiApplicationTests {
 
+    @Autowired
+    private OpenAiEmbeddingModel embeddingModel;
+
     @Test
     void contextLoads() {
+        // 1.测试数据
+        // 1.1.用来查询的文本，国际冲突
+        String query = "global conflicts";
+
+        // 1.2.用来做比较的文本
+        String[] texts = new String[]{
+                "哈马斯称加沙下阶段停火谈判仍在进行 以方尚未做出承诺",
+                "土耳其、芬兰、瑞典与北约代表将继续就瑞典“入约”问题进行谈判",
+                "日本航空基地水井中检测出有机氟化物超标",
+                "国家游泳中心（水立方）：恢复游泳、嬉水乐园等水上项目运营",
+                "我国首次在空间站开展舱外辐射生物学暴露实验",
+        };
+        // 2.向量化
+        // 2.1.先将查询文本向量化
+        float[] queryVector = embeddingModel.embed(query);
+
+        // 2.2.再将比较文本向量化，放到一个数组
+        List<float[]> textVectors = embeddingModel.embed(Arrays.asList(texts));
+
+        // 3.比较欧氏距离
+        // 3.1.把查询文本自己与自己比较，肯定是相似度最高的
+        System.out.println(VectorDistanceUtils.euclideanDistance(queryVector, queryVector));
+        // 3.2.把查询文本与其它文本比较
+        for (float[] textVector : textVectors) {
+            System.out.println(VectorDistanceUtils.euclideanDistance(queryVector, textVector));
+        }
+        System.out.println("------------------");
+
+        // 4.比较余弦距离
+        // 4.1.把查询文本自己与自己比较，肯定是相似度最高的
+        System.out.println(VectorDistanceUtils.cosineDistance(queryVector, queryVector));
+        // 4.2.把查询文本与其它文本比较
+        for (float[] textVector : textVectors) {
+            System.out.println(VectorDistanceUtils.cosineDistance(queryVector, textVector));
+        }
     }
 
 }
